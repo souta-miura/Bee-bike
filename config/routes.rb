aa1f08b2b4fe295f2f4e8f3844bad629642f3397
EC2 Default User <ec2-user@ip-172-31-12-184.ap-northeast-1.compute.internal>
2022-08-18 07:17:35 +0000

    add routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,47 +1,6 @@
 Rails.application.routes.draw do
-  
-  namespace :user do
-    get 'users/index'
-    get 'users/show'
-    get 'users/edit'
-    get 'users/quit'
-  end
-  namespace :admin do
-    get 'genres/index'
-    get 'genres/edit'
-  end
-  namespace :admin do
-    get 'items/index'
-    get 'items/show'
-    get 'items/edit'
-  end
-  namespace :admin do
-    get 'users/index'
-    get 'users/show'
-    get 'users/edit'
-  end
   root 'user/homes#top'
   
-  namespace :user do
-    get 'addresses/index'
-    get 'addresses/edit'
-  end
-  namespace :user do
-    get 'orders/new'
-    get 'orders/comp'
-    get 'orders/index'
-    get 'orders/show'
-  end
-  namespace :user do
-    get 'homes/top'
-    get 'homes/about'
-  end
-  namespace :user do
-    get 'items/new'
-    get 'items/index'
-    get 'items/show'
-    get 'items/edit'
-  end
   devise_for :user,skip: [:passwords], controllers: {
   registrations: "user/registrations",
   sessions: 'user/sessions'
@@ -50,5 +9,32 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
   }
+  
+  #ユーザー用
+  namespace :user, :path => '' do
+    get '/about'=>'homes#about'
+    
+    resources :users, only: [:index, :show, :edit, :update]
+    get 'users/:id/quit'
+    patch 'users/remove'
+    
+    resources :addresses, only: [:index, :create, :edit, :update, :destroy]
+    
+    resources :orders, only: [:new, :create, :index, :show]
+    get 'orders/:id/comp'=>'orders#comp'
+    post 'orders/info'=>'orders#info'
+    
+    resources :items, only: [:new, :create, :index, :show, :edit, :update]
+  end
+  
+  #管理者用
+  namespace :admin do
+    resources :users, only: [:index, :show, :edit, :update]
+    
+    resources :genres, only: [:index, :create, :edit, :update]
+    
+    resources :items, only: [:index, :show, :edit, :update]
+  end
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
