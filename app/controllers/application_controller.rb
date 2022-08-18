a5ef361a8bd60349443a595adf07a2c97586f18d
EC2 Default User <ec2-user@ip-172-31-12-184.ap-northeast-1.compute.internal>
2022-08-18 07:42:25 +0000

    add user sign_up,in user_mypage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+    before_action :conigure_permitted_parameters, if: :devise_controller?
+    
+    def after_sign_in_path_for(resource)
+        case resource
+        when Admin
+            admin_path
+        when User
+            root_path
+        end
+    end
+    
+    
+    protected
+    
+    def conigure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :encrypted_password, :last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :phone_number])
+    end
 end
--- app/controllers/user/users_controller.rb
+++ app/controllers/user/users_controller.rb
@@ -3,11 +3,27 @@ class User::UsersController < ApplicationController
   end
 
   def show
+    @user=User.find(params[:id])
   end
 
   def edit
+    @user=User.find(params[:id])
   end
+  
+  def update
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end 
 
   def quit
+  end
+  
+  def remove
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:id, :last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :phone_number, :email, :is_deleted)
   end
 end
