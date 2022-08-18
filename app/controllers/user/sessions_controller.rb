43e2b78e2226665341f1b4677591e73df9eea570
EC2 Default User <ec2-user@ip-172-31-12-184.ap-northeast-1.compute.internal>
2022-08-18 08:34:19 +0000

    add 退会機能

--- app/controllers/user/sessions_controller.rb
+++ app/controllers/user/sessions_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class User::SessionsController < Devise::SessionsController
+  before_action :user_state, only: [:create]
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
@@ -18,7 +19,19 @@ class User::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
+  # 退会しているかを判断するメソッド
+  def user_state
+    ## 【処理内容1】 入力されたemailからアカウントを1件取得
+    @user = User.find_by(email: params[:user][:email])
+    ## アカウントを取得できなかった場合、このメソッドを終了する
+    return if !@user
+    ## 【処理内容2】 取得したアカウントのパスワードと入力されたパスワードが一致してるかを判別
+    if @user.valid_password?(params[:user][:password]) && (@user.is_deleted == true)
+    ## 【処理内容3】
+    redirect_to new_user_session_path
+    end
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
--- app/controllers/user/users_controller.rb
+++ app/controllers/user/users_controller.rb
@@ -1,4 +1,6 @@
 class User::UsersController < ApplicationController
+  before_action :ensure_correct_user, only: [:edit, :update]
+  
   def index
   end
 
@@ -19,11 +21,21 @@ class User::UsersController < ApplicationController
   end
   
   def remove
+    current_user.update(is_deleted: true)
+    reset_session
+    redirect_to root_path
   end
   
   private
   
   def user_params
     params.require(:user).permit(:id, :last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :phone_number, :email, :is_deleted)
+  end
+  
+  def ensure_correct_user
+    @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to user_user_path(current_user)
+    end
   end
 end
