3a3ecb469c29e871db80e65a2916141b62c6b0c6
soutamiura <dondongongon2525@gmail.com>
2022-09-19 17:26:10 +0000

    add searchの記述をitem modelとuser model内に記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,22 @@
 class Item < ApplicationRecord
-    has_one_attached :image
-    
-    belongs_to :user
-    belongs_to :genre
-    belongs_to :maker
-    belongs_to :cc_class
-    enum sale_status: { 販売停止中: 0, 販売中: 1 }
-    enum sale_method: { 引き渡しのみ: 0, 配送のみ: 1, 配送と引き渡し: 2 }
-    
-    validates :name, presence: true
-    validates :description, presence: true
-    validates :price, presence: true
+  has_one_attached :image
+  
+  belongs_to :user
+  belongs_to :genre
+  belongs_to :maker
+  belongs_to :cc_class
+  enum sale_status: { 販売停止中: 0, 販売中: 1 }
+  enum sale_method: { 引き渡しのみ: 0, 配送のみ: 1, 配送と引き渡し: 2 }
+  
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+  
+  def self.search_for(content, method)
+    if method == 'perfect'
+      Item.where(name: content)
+    else
+      Item.where('name LIKE ?', '%'+content+'%')
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -20,10 +20,6 @@ class User < ApplicationRecord
   def self.search_for(content, method)
     if method == 'perfect'
       User.where(name: content)
-    elsif method == 'forward'
-      User.where('name LIKE ?', content + '%')
-    elsif method == 'backward'
-      User.where('name LIKE ?', '%' + content)
     else
       User.where('name LIKE ?', '%' + content + '%')
     end
