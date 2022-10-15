f1b9903828270d21d35b2ef5faccb2eabcfa7274
soutamiura <dondongongon2525@gmail.com>
2022-10-15 17:42:23 +0000

    homes/topジャンル検索controller修正

--- app/controllers/user/homes_controller.rb
+++ app/controllers/user/homes_controller.rb
@@ -2,6 +2,7 @@ class User::HomesController < ApplicationController
   def top
     @item=Item.limit(4).order("created_at DESC")
     @genres=Genre.all
+    @genre=Genre.find(params[:id])
     @makers=Maker.all
     @cc_classes=CcClass.all
   end
